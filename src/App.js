5508a1e1e46a2ea51b8a61ecefaf59d246015c52
shymaSane <shymaSane1@gmail.com>
2021-09-10 09:42:33 +0300

    [add]starinitial region

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import {Text, View, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import styles from './Style';
 
+const App = () => {
+  return (
+    <SafeAreaView style={styles.mapContainer}>
+      <StatusBar barStyle="light-content" />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 31.85083048650159,
+          longitude: 36.031689212485695,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default class App extends Component {
-  render() {
-    return (
-      <SafeAreaView>
-        <StatusBar barStyle = "dark-content"/>
-      </SafeAreaView>
-    );
-  }
-}
+export default App;
---
+++ src/Style.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default styles;
